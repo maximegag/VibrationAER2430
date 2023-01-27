585557caec3e53a7bdbab1eb6e7b63ed17fbd6e1
maximegag <60531874+maximegag@users.noreply.github.com>
2023-01-26 21:50:47 -0500

    Update Laboratoire 1-2c.py

--- Laboratoire 1-2c.py
+++ Laboratoire 1-2c.py
@@ -31,6 +31,9 @@ Ceq_ed = 2*C_ed*Wn
 sol1_0 = [0,omega_idot]
 sol2_0 = [0,omega_idot]
 sol3_0 = [0,omega_idot]
+sol4_0 = [0,omega_idot]
+sol5_0 = [0,omega_idot]
+sol6_0 = [0,omega_idot]
 
 
 w50 = 50
@@ -57,11 +60,21 @@ sol_op2[0]=0
 sol_op3 = np.empty(len(t),)
 sol_op3[0]=0
 
+sol_ed1 = np.empty(len(t),)
+sol_ed1[0]=0
+sol_ed2 = np.empty(len(t),)
+sol_ed2[0]=0
+sol_ed3 = np.empty(len(t),)
+sol_ed3[0]=0
+
 for i in range(0,len(t)-1):
     dt = [t[i],t[i+1]]
     sol1 = odeint(model,sol1_0,dt,args=(M50[i],Ceq_op,I,K))
     sol2 = odeint(model,sol2_0,dt,args=(M100[i],Ceq_op,I,K))
     sol3 = odeint(model,sol3_0,dt,args=(Mwn[i],Ceq_op,I,K))
+    sol4 = odeint(model,sol4_0,dt,args=(M50[i],Ceq_ed,I,K))
+    sol5 = odeint(model,sol5_0,dt,args=(M100[i],Ceq_ed,I,K))
+    sol6 = odeint(model,sol6_0,dt,args=(Mwn[i],Ceq_ed,I,K))
     
     sol1_0 = sol1[1]
     sol2_0 = sol2[1]
@@ -69,29 +82,29 @@ for i in range(0,len(t)-1):
     sol_op1[i+1]=sol1[1,0]
     sol_op2[i+1]=sol2[1,0]
     sol_op3[i+1]=sol3[1,0]
+    
+    sol4_0 = sol4[1]
+    sol5_0 = sol5[1]
+    sol6_0 = sol6[1]
+    sol_ed1[i+1]=sol4[1,0]
+    sol_ed2[i+1]=sol5[1,0]
+    sol_ed3[i+1]=sol6[1,0]
 
-sol_op50 = plt.plot(t, sol_op1, label='W = 50')
-sol_op100 = plt.plot(t, sol_op2, label='W = 100')
-sol_opwn1 = plt.plot(t, sol_op3, label='W = Wn')
 
 
-#sol_ed50 = plt.plot(t, sol_op1, label='W = 50')
-#sol_ed100 = plt.plot(t, sol_op2, label='W = 100')
-#sol_edwn1 = plt.plot(t, sol_op3, label='W = Wn1')
 
-plt.title("Oscillation de l'aileron")
+#sol_op50 = plt.plot(t, sol_op1, label='w = 50.00 rad/s')
+#sol_op100 = plt.plot(t, sol_op2, label='w = 100.00 rad/s')
+#sol_opwn1 = plt.plot(t, sol_op3, label='w = 252.98 rad/s')
+
+
+sol_ed50 = plt.plot(t, sol_ed1, label='W = 50.00')
+sol_ed100 = plt.plot(t, sol_ed2, label='W = 100.00')
+sol_edwn1 = plt.plot(t, sol_ed3, label='W = 252.98')
+
+plt.title("Oscillation de l'aileron calculée avec ζ en défaillance")
 plt.xlabel('Temps (s)')
 plt.ylabel("Angle de l'aileron (rad)")
 plt.legend(loc="upper right")
-plt.show(sol_op50, sol_op100,sol_opwn1)
-
-
-
-
-
-
-
-
-
-
-
+#plt.show(sol_op50, sol_op100,sol_opwn1)
+plt.show(sol_ed50, sol_ed100,sol_edwn1)
